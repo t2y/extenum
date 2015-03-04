1ab09fbadc2c0a07d67f1592920fb8b21725b800
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2015-03-05 07:30:51 +0900

    Updated the version 0.6.0

--- extenum/__init__.py
+++ extenum/__init__.py
@@ -3,4 +3,4 @@ from .constant_specific import ConstantSpecificEnum
 from .constant_specific import RegisterFactory
 from .implicit_enum import ImplicitEnum
 
-__version__ = '0.5.0'
+__version__ = '0.6.0'
